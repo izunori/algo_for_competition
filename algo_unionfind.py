fe838b5d435d5adad4d077990ec4c07cd9c39c02
izunori <izuharakun53@yahoo.co.jp>
2021-08-05 22:13:02 +0900

    Add algo_unionfind.py

---
+++ algo_unionfind.py
@@ -0,0 +1,43 @@
+# Union Find
+from collections import defaultdict
+class UnionFind:
+    def __init__(self,nodes):
+        self.parents = {node : node for node in nodes}
+        self.num = {node : 1 for node in self.parents.keys()}
+    def root(self,x):
+        if x == self.parents[x]:
+            return x
+        else:
+            rt = self.root(self.parents[x])
+            self.parents[x] = rt
+            return rt
+    def find(self,x,y):
+        return self.root(x) == self.root(y)
+    def union(self,x,y):
+        rx = self.root(x)
+        ry = self.root(y)
+        if rx == ry:
+            return
+        if self.num[ry] >= self.num[rx]:
+            self.num[ry] += self.num[rx]
+            self.parents[rx] = ry
+        else:
+            self.num[rx] += self.num[ry]
+            self.parents[ry] = rx
+    def get(self):
+        res = defaultdict(list)
+        for node in self.parents:
+            res[self.root(node)].append(node)
+        return list(res.values())
+
+def test():
+    samples = range(10)
+    uf = UnionFind(samples)
+    uf.union(0,8)
+    uf.union(0,7)
+    uf.union(1,5)
+    print(uf.find(8,7))
+    print(uf.get())
+
+if __name__=='__main__':
+    test()
