b52b0cec70d90b3a36eebc440b5ebef7c65854ec
izunori <izuharakun53@yahoo.co.jp>
2022-02-26 20:16:27 +0900

    Add BitVector

--- algo_WaveletMatrix.py
+++ algo_WaveletMatrix.py
@@ -1,4 +1,53 @@
 import array as ar
+from itertools import accumulate
+import math
+class SimpleBitVector:
+    def __init__(self, data):
+        self.n = len(data)
+        self.s = int(math.log(self.n))//2
+        self.l = 4*self.s**2
+        self.data = data
+        self.ldata = []
+        self.sdata = []
+        cnt = 0
+        scnt = 0
+        for i,x in enumerate(self.data):
+            if i % self.l == 0:
+                scnt = 0
+                self.ldata.append(cnt)
+            if i % self.s == 0:
+                self.sdata.append(scnt)
+            if x == 1:
+                cnt += 1
+                scnt += 1
+    def access(self, i):
+        return self.data[i]
+    def rank(self, i, x):
+        p = self.s * (i//self.s)
+        n1 = self.ldata[i//self.l] + self.sdata[i//self.s] + sum(self.data[p:i])
+        return n1 if x == 1 else i-n1
+    def select(self, i, x):
+        # TODO
+        return 0
+class BitVector:
+    def __init__(self, data):
+        self.n = len(data)
+        self.data = data
+        self.cumsum = [0] + list(accumulate(data))
+    def access(self, i):
+        return self.data[i]
+    def rank(self, i, x):
+        return self.cumsum[i] if x else i - self.cumsum[i]
+    def select(self, i, x):
+        l,r = 0, self.n
+        while r-l > 1:
+            m = (r+l)//2
+            n = self.cumsum[m] if x else m - self.cumsum[m]
+            if n < i:
+                l = m
+            else:
+                r = m
+        return l
 class WaveletMatrix:
     def __init__(self, data):
         self.T = data[:]
@@ -81,8 +130,8 @@ class WaveletMatrix:
             th >>= 1
         return self.T[r-1]
 
-
-if __name__=='__main__':
+def test():
+    print("- Test WaveletMatrix")
     #       0,1,2,3,4,5,6,7,8,9,0,1
     data = [5,4,5,5,2,1,5,6,1,3,5,0]
     wm = WaveletMatrix(data)
@@ -95,4 +144,37 @@ if __name__=='__main__':
     print(wm.select(4,5),10)
     print(wm.select(0,3),9)
     print(wm.quantile(1,11,5),5)
+def test2():
+    print("- Test SimpleBitVector")
+    #       0,1,2,3,4,5,6,7
+    data = [1,1,0,0,1,1,1,0]
+    #sbv = SimpleBitVector(data)
+    sbv = BitVector(data)
+    print(sbv.access(1),1)
+    print(sbv.rank(5,0),2)
+    print(sbv.rank(5,1),3)
+    print(sbv.select(1,0),2)
+    print(sbv.select(2,0),3)
+    print(sbv.select(3,0),7)
+    print(sbv.select(1,1),0)
+    print(sbv.select(2,1),1)
+    print(sbv.select(3,1),4)
+    print(sbv.select(4,1),5)
+    print(sbv.select(5,1),6)
 
+def perf():
+    print("- Test Performance")
+    from time import perf_counter as time
+    import random
+    N = 2*10**5
+    M = 10
+    data = [random.randint(0,M) for _ in range(N)]
+    wm = WaveletMatrix(data)
+    start = time()
+    wm.select(1000,1)
+    print(f"{time()-start}s")
+
+if __name__=='__main__':
+    test()
+    test2()
+    perf()
