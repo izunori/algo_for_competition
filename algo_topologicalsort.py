4f392ec487cc9b60cc855983080b9eec8ef77081
izunori <izuharakun53@yahoo.co.jp>
2022-03-29 22:21:46 +0900

    Fix TopologicalSort

--- algo_topologicalsort.py
+++ algo_topologicalsort.py
@@ -1,9 +1,9 @@
 from collections import deque, defaultdict
-def TopologicalSort(g, N):
-    n_parent = [0]*(N+1)
+def TopologicalSort(g):
+    n_parent = defaultdict(int)
     res = []
-    for v in g:
-        for nv in g[v]:
+    for nvs in g.values():
+        for nv in nvs:
             n_parent[nv] += 1
     dq = deque([v for v in g if n_parent[v] == 0])
     while dq:
@@ -13,18 +13,18 @@ def TopologicalSort(g, N):
             n_parent[nv] -= 1
             if n_parent[nv] == 0:
                 dq.append(nv)
-    # if len(res) < n of v -> loop
+    # if any(n_parent), g has a cycle
     return res
 
 def test():
     g = {
         1:[2,3], 2:[3,4,5], 3:[5], 4:[6], 5:[4,6], 6:[]
     }
-    print(TopologicalSort(g,6))
+    print(TopologicalSort(g))
     g = {
         1:[2], 2:[3], 3:[4], 4:[1], 5:[1]
     }
-    print(TopologicalSort(g,5)) # cycle
+    print(TopologicalSort(g)) # cycle
 
 if __name__=='__main__':
     test()
