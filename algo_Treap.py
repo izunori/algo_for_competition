594f2ace30b2ed0f6dab80c7625047fe5ae6049a
izunori <izuharakun53@yahoo.co.jp>
2022-04-13 23:18:47 +0900

    Implement KthSmall in Treap

--- algo_Treap.py
+++ algo_Treap.py
@@ -1,164 +1,4 @@
-class TreapTuple:
-    # (key, priority, lch, rch)
-    def __init__(self):
-        self.root = None
-    def merge(self, a, b):
-        if a is None or b is None:
-            return a if b is None else b
-        if a[1] > b[1]:
-            a[3] = self.merge(a[3],b)
-            return self.update(a)
-        else:
-            b[2] = self.merge(a,b[2])
-            return self.update(b)
-    def update(self, t):
-        t[4] = (t[3][4] if t[3] else 0) + (t[2][4] if t[2] else 0) + 1
-        t[5] = (t[3][5] if t[3] else 0) + (t[2][5] if t[2] else 0) + t[0]
-        return t
-    def split(self, t, k):
-        if t is None:
-            return (None, None)
-        if k <= t[0]:
-            s = self.split(t[2], k)
-            t[2] = s[1]
-            return s[0], t
-        else:
-            s = self.split(t[3], k)
-            t[3] = s[0]
-            return t, s[1]
-    def insert(self, k):
-        lt, rt = self.split(self.root, k)
-        node = [k, random.random(), None, None, 1, k]
-        self.root = self.merge(self.merge(lt, node), rt)
-        return self
-    def remove(self, k):
-        lt,rt = self.split(self.root, k)
-        _,rt = self.split(rt, k+1)
-        self.root = self.merge(lt, rt)
-
-    def show(self):
-        from collections import deque
-        if self.root is None:
-            print(None)
-        dq = deque([self.root])
-        while dq:
-            t = dq.popleft()
-            vs = []
-            if t[2]:
-                dq.append(t[2])
-                vs.append(t[2][0])
-            else:
-                vs.append(None)
-            if t[3]:
-                dq.append(t[3])
-                vs.append(t[3][0])
-            else:
-                vs.append(None)
-            print(t[0],"->",vs)
-    def search(self, k):
-        t = self.root
-        while True:
-            if t is None:
-                return False
-            if t[0] == k:
-                return True
-            t = t[3] if t[0] < k else t[2]
-
 import random
-class TreapRecursive:
-    class Node:
-        def __init__(self, key):
-            self.l, self.r = None, None
-            self.key = key
-            self.pri = random.random()
-            self.cnt = 1
-            self.sum = key
-            self.depth = 0
-        def __str__(self):
-            return f"{str(self.key)}->({self.l},{self.r})"
-    def __init__(self, t = None):
-        self.root = t
-    def update(self, t):
-        t.cnt = (t.r.cnt if t.r else 0) + (t.l.cnt if t.l else 0) + 1
-        t.sum = (t.r.sum if t.r else 0) + (t.l.sum if t.l else 0) + t.key
-        t.depth = max((t.r.depth if t.r else 0), (t.l.depth if t.l else 0))+1
-        return t
-    def merge(self, a, b):
-        if a is None or b is None:
-            return a if b is None else b
-        if a.pri > b.pri:
-            a.r = self.merge(a.r, b)
-            return self.update(a)
-        else:
-            b.l = self.merge(a, b.l)
-            return self.update(b)
-    def split(self, t, k):
-        if t is None:
-            return (None, None)
-        if k <= t.key:
-            l,t.l = self.split(t.l, k)
-            return (l, t)
-        else:
-            t.r,r = self.split(t.r, k)
-            return t, r
-    def insert(self, k):
-        lt, rt = self.split(self.root, k)
-        self.root = self.merge(self.merge(lt, self.Node(k)), rt)
-        return self
-    def remove(self, k):
-        lt,rt = self.split(self.root, k)
-        _,rt = self.split(rt, k+1)
-        self.root = self.merge(lt, rt)
-    def search(self, k):
-        t = self.root
-        while True:
-            if t is None:
-                return False
-            if t.key == k:
-                return True
-            t = t.r if t.key < k else t.l
-    def findEqualOrGreaterThan(self, x):
-        t = self.root
-        res = None
-        while True:
-            if t is None:
-                return res
-            if x <= t.key:
-                res = t.key
-                t = t.l
-            else:
-                t = t.r
-    def findEqualOrLessThan(self, x):
-        t = self.root
-        res = None
-        while True:
-            if t is None:
-                return res
-            if t.key <= x:
-                res = t.key
-                t = t.r
-            else:
-                t = t.l
-
-    def show(self):
-        from collections import deque
-        if self.root is None:
-            print(None)
-        dq = deque([self.root])
-        while dq:
-            t = dq.popleft()
-            vs = []
-            if t.l:
-                dq.append(t.l)
-                vs.append(t.l.key)
-            else:
-                vs.append(None)
-            if t.r:
-                dq.append(t.r)
-                vs.append(t.r.key)
-            else:
-                vs.append(None)
-            print(t.key,"->",vs)
 
 ## omake
 y = 2463534242
@@ -171,6 +11,7 @@ def xorShift():
 
 class Treap:
     class Node:
+        __slots__ = ['l','r','key','pri','cnt','sum','depth']
         def __init__(self, key):
             self.l, self.r = None, None
             self.key = key
@@ -205,7 +46,7 @@ class Treap:
             else:
                 x.l,t = t,x
         return self.update(t)
-    def split(self, t, k):
+    def split(self, t, k): # (:k)[k:)
         pairs = []
         while t is not None:
             if k <= t.key:
@@ -260,7 +101,57 @@ class Treap:
             else:
                 t = t.l
         return res
+    def findGreaterKth(self,x,k):
+        _,rt = self.split(self.root, k)
+        c = 0
+        nk = k
+        while c < k:
+            lt,rt = self.split(rt,nk+1)
+            if rt is None:
+                return None
+            if lt is not None:
+                c += lt.cnt
+            nk += 1
+        return nk-1
+    def findKthSmallest(self,k,than=None): # 0-index
+        if than is None:
+            x = self.root
+        else:
+            lt,rt = self.split(self.root, than)
+            x = rt
+        st = [(x,0)]
+        while st and k >= 0:
+            x,s = st.pop()
+            if x is None:
+                continue
+            if s == 0:
+                st.append((x,1))
+                st.append((x.l,0))
+            elif s == 1:
+                k -= 1
+                st.append((x,2))
+                st.append((x.r,0))
+        if than:
+            self.root = self.merge(lt,rt)
+        if not st:
+            return None
+        return x.key
 
+    # for debug
+    def findKthSmallestRecursive(self,k):
+        self.k = k
+        t = self._findKthSmallestRecursive(self.root)
+        return t.key if t else None
+    def _findKthSmallestRecursive(self,x):
+        if x is None:
+            return None
+        left = self._findKthSmallestRecursive(x.l)
+        if left is not None:
+            return left
+        self.k -= 1
+        if self.k == 0:
+            return x
+        return self._findKthSmallestRecursive(x.r)
     def show(self):
         from collections import deque
         if self.root is None:
@@ -292,7 +183,7 @@ def test():
     trp.insert(-10)
     trp.remove(20)
     trp.remove(10)
-    trp.show()
+    trp.show() # -10,5,40
     print(trp.findEqualOrGreaterThan(4),5)
     print(trp.findEqualOrGreaterThan(5),5)
     print(trp.findEqualOrGreaterThan(6),40)
@@ -302,6 +193,8 @@ def test():
     print(trp.findEqualOrLessThan(39),5)
     print(trp.findEqualOrLessThan(40),40)
     print(trp.findEqualOrLessThan(41),40)
+    print(trp.findKthSmallest(0),-10)
+    print(trp.findKthSmallest(1,5),40)
 
 def perf():
     print("---PERF")
@@ -331,6 +224,44 @@ def perf():
     print(trp.root.l.depth,trp.root.r.depth)
     print(trp.root.l.l.depth,trp.root.l.r.depth, trp.root.r.l.depth,trp.root.r.r.depth)
 
+def testRandom():
+    import random
+    N = 100
+    K = 100
+    samples = [random.randint(-K,K) for i in range(N)]
+    treap = Treap()
+    for s in samples:
+        treap.insert(s)
+    samples.sort()
+    for i,s in enumerate(samples):
+        a = treap.findKthSmallestRecursive(i+1)
+        b = treap.findKthSmallest(i)
+        if len(set([a,b,s])) > 1:
+            print("ERROR")
+
+def testPerf():
+    import random
+    from time import perf_counter as time
+    N = 2*10**5
+    K = 10**9
+    samples = [random.randint(-K,K) for i in range(N)]
+    ks = [random.randint(1,10) for i in range(N)]
+    treap = Treap()
+    for s in samples:
+        treap.insert(s)
+    samples.sort()
+    start = time()
+    for k in ks:
+        treap.findKthSmallestRecursive(k)
+    print(f"{time() - start}")
+
+    start = time()
+    for k in ks:
+        treap.findKthSmallest(k)
+    print(f"{time() - start}")
+
 if __name__=='__main__':
+    #testRandom()
+    #testPerf()
     test()
-    perf()
+    #perf()
