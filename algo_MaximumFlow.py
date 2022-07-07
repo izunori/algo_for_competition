c52b45d663761c567c85b9c682dd75f395a36fd7
izunori <izuharakun53@yahoo.co.jp>
2022-07-07 23:13:22 +0900

    Implement MaximumFlow class

--- algo_MaximumFlow.py
+++ algo_MaximumFlow.py
@@ -1,5 +1,65 @@
 from collections import defaultdict, deque
 
+class MaximumFlow:
+    def __init__(self, N):
+        self.N = N
+        self.g = [[] for _ in range(N)]
+        self.c = [[0]*N for _ in range(N)]
+        self.inf = 2**64
+    def add(self,u,v,c):
+        self.g[u].append(v)
+        self.g[v].append(u)
+        self.c[u][v] = c # Be careful about multiple edges
+        self.c[v][u] = -c
+    def bfs(self,s):
+        dq = deque([s])
+        self.d = [self.inf]*self.N
+        self.d[s] = 0
+        while dq:
+            v = dq.popleft()
+            for nv in self.g[v]:
+                if not self.c[v][nv] > 0:
+                    continue
+                if self.d[v]+1 < self.d[nv]:
+                    self.d[nv] = self.d[v]+1
+                    dq.append(nv)
+    def dfs(self,s,t):
+        st = [s]
+        p = [-1]*self.N
+        while st:
+            v = st.pop()
+            if v == t:
+                break
+            for nv in self.g[v]:
+                if not (self.c[v][nv] > 0 and self.d[v] < self.d[nv]):
+                    continue
+                p[nv] = v
+                st.append(nv)
+        else:
+            return 0
+        route = [t]
+        while route[-1] != s:
+            t = p[t]
+            route.append(t)
+        route.reverse()
+        f = min(self.c[u][v] for u,v in zip(route,route[1:]))
+        for u,v in zip(route,route[1:]):
+            self.c[u][v] -= f
+            self.c[v][u] += f
+        return f
+    def maxFlow(self,s,t):
+        flow = 0
+        while True:
+            self.bfs(s)
+            if self.d[t] == self.inf:
+                return flow
+            while True:
+                f = self.dfs(s,t)
+                if f > 0:
+                    flow += f
+                else:
+                    break
+
 def FordFulkerson(g,s,t):
     flow = 0
     while True:
@@ -105,15 +165,18 @@ def test():
     g = defaultdict(dict)
     g2 = defaultdict(list)
     cap = defaultdict(int)
+    gr = MaximumFlow(21)
     for u,v,c in UVC:
         g[u][v] = c
         g[v][u] = 0
         g2[u].append(v)
         g2[v].append(u)
         cap[(u,v)] = c
+        gr.add(u,v,c)
     print(FordFulkerson(g,0,5), 23)
     print(FordFulkerson2(g2,cap,0,5), 23)
     print(EdmondsKarp(g,cap,0,5), 23)
+    print(gr.maxFlow(0,5))
 
 if __name__=='__main__':
     test()
