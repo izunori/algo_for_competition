ce9b6fb3848b4f3ad5bac3777e915cab26136045
izunori <izuharakun53@yahoo.co.jp>
2023-03-09 23:52:44 +0900

    Refactor

--- algo_SA_IS.py
+++ algo_SA_IS.py
@@ -14,22 +14,19 @@ def SA_IS(S):
             ls[i] = 0 # L
         else:
             ls[i] = ls[i+1]
-    lms = []
-    for i in range(N+1):
-        if ls[i] == 0 and ls[i+1] == 1:
-            lms.append(i+1)
+    lms = [i+1 for i in range(N+1) if not ls[i] and ls[i+1]]
+    M = len(lms)
     lms_subs = {N:''}
-    for i,j in zip(lms,lms[1:]):
-        lms_subs[i] = S[i:j+1]
-
+    for i in range(M-1):
+        lms_subs[lms[i]] = S[lms[i]:lms[i+1]+1]
     def induced_sort(lms):
         rank = [-1]*(N+1)
         rank[0] = N
-        d = defaultdict(lambda:[0,0])
+        d = {s:[0,0] for s in ss}
         for s,x in zip(S,ls):
             d[s][x] += 1
         d[''][1] = 1
-        d2 = defaultdict(lambda:[-1,-1])
+        d2 = {s:[None,None] for s in ss}
         i = 0
         for s in ss:
             for x in range(2):
@@ -59,19 +56,15 @@ def SA_IS(S):
                 d2[s][1][2] -= 1
         return rank
     rank = induced_sort(lms)
-    lms2 = []
-    for i in rank:
-        if i > 0 and ls[i-1] == 0 and ls[i] == 1:
-            lms2.append(i)
+    lms2 = [i for i in rank if i > 0 and not ls[i-1] and ls[i]]
     nums = [0]
-    for i,j in zip(lms2,lms2[1:]):
-        if lms_subs[i] == lms_subs[j]:
+    for i in range(M-1):
+        if lms_subs[lms2[i]] == lms_subs[lms2[i+1]]:
             nums.append(nums[-1])
         else:
             nums.append(nums[-1]+1)
     m = {x:n for x,n in  zip(lms2,nums)}
-    nxt = [m[i] for i in lms]
-    res = SA_IS(nxt)
+    res = SA_IS([m[i] for i in lms])
     new_lms = [lms[i] for i in res[1:]]
     return induced_sort(new_lms)
         
@@ -83,6 +76,10 @@ def for_test(S):
 
 def test():
     print("--test")
+    S = 'mmiissiissiippii'
+    print(SA_IS(S))
+    print(for_test(S))
+
     import random
     N = 100
     M = 100
@@ -98,10 +95,11 @@ def test():
     print("ok")
 
 def perf():
+    print("--perf")
     import random
     from time import perf_counter as time
-    N = 2*10**5
-    S = "".join([chr(97+random.randint(0,1)) for _ in range(N)])
+    N = 10*10**5
+    S = "".join([chr(97+random.randint(0,26)) for _ in range(N)])
 
     start=time()
     SA_IS(S)
@@ -113,11 +111,6 @@ def perf():
 
 
 if __name__=='__main__':
-    #S = 'mmiissiissiippii'
-    #T = list(map(ord, S))
-    #res = SA_IS(S)
-    #print(res)
-    #print(for_test(S))
     test()
     perf()
     
