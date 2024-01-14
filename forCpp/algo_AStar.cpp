6ac3adeccc3a840c544e43a03ba5a740e3d81c41
izunori <izuharakun53@yahoo.co.jp>
2024-01-14 18:25:25 +0900

    Impl A-Star

--- forCpp/algo_AStar.cpp
+++ forCpp/algo_AStar.cpp
@@ -13,42 +13,33 @@
 #include<stack>
 #include<cassert>
 #include<cmath>
+#include"dprint.h"
 
-#define rep(i, n) for (int i = 0; i < (int)n; i++)
-#define rep2(i,j,n) for (int i = 0; i < (int)n; i++) for (int j = 0; j < (int)n; j++)
-#define rep3(i,j,k,n) for (int i = 0; i < (int)n; i++) for (int j = 0; j < (int)n; j++) for (int k= 0; k < (int)n; k++)
-#define all(v) v.begin(),v.end()
-
-template<typename T>
-using vec = std::vector<T>;
-template<typename T>
-using vec2 = vec<vec<T>>;
-using ll = long long;
-template<typename T>
-using t2 = std::tuple<T, T>;
-using i2 = t2<int>;
-template<typename T>
-using t3 = std::tuple<T, T, T>;
-using i3 = t3<int>;
-using clk = std::chrono::system_clock;
-using i4 = std::tuple<int,int,int,int>;
-
-template<typename T>
-using vec = std::vector<T>;
-
-int N = 100;
-
-class AStar{
-
-};
-
+int N = 10;
 
 int index(int x, int y){
     return x*N + y;
 }
+i2 pos(int i){
+    return {i/N, i%N};
+}
+
+int h(int v){
+    // sholt be 0 <= h(v) <= true h(v)
+    const auto [x,y] = pos(v);
+    int nx = N/2;
+    int ny = N/2;
+    return (std::abs(x-nx) + std::abs(y-ny))/2;
+}
+
+int cost(int v, int nv){
+    const auto [x,y] = pos(v);
+    const auto [nx,ny] = pos(nv);
+    return std::abs(x-nx) + std::abs(y-ny);
+}
 
 int main(){
-    vec2<int> g(N, vec<int>());
+    vec2<int> graph(N*N, vec<int>());
     vec<i2> dir{{1,0},{0,1},{-1,0},{0,-1}};
     rep(x,N){
         rep(y,N){
@@ -58,8 +49,50 @@ int main(){
                 int ny = y+dy;
                 if(!(0<=nx && nx < N && 0 <= ny && ny < N)) continue;
                 int ni = index(nx,ny);
-                g[i].push_back(ni);
+                graph[i].push_back(ni);
             }
         }
+    }
+
+    // open: generated (neighbors of expanded(closed) nodes)
+    // closed: expanded (already searched)
+    //         If you find more efficient path, closed will be open.
+    // neither: to be.
+    // if cost is already 1 => variable array for each f value.
+    vec<bool> isClose(N*N,false);
+    int inf = 99;
+    vec<int> f(N*N,inf);
+    std::priority_queue<i2,vec<i2>,std::greater<i2>> open;
+    int s = index(0,0);
+    int t = index(N/2,N/2);
+    f[s] = h(s);
+    open.emplace(f[s],s);
+    vec<int> parent(N*N,-1);
+    while(!open.empty()){
+        const auto [fv,v] = open.top();
+        dprint(fv,v);
+        open.pop();
+        if(f[v] < fv || isClose[v]) continue;
+        if(v == t) break;
+        isClose[v] = true;
+        for(const auto nv : graph[v]){
+            int fnv = (f[v] - h(v)) + cost(v,nv) + h(nv);
+            open.emplace(fnv, nv);
+            if(fnv < f[nv]){
+                f[nv] = fnv;
+                parent[nv] = v;
+                if(isClose[nv]){
+                    isClose[nv] = false;
+                }
+            }
+        }
+    }
+
+    dprint(f[t] - h(t));
+    rep(x,N){
+        rep(y,N){
+            std::cout << f[index(x,y)] - h(index(x,y)) << " ";
+        }
+        std::cout << std::endl;
     }
 }
---
+++ forCpp/dprint.h
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<tuple>
+#include<chrono>
+#include<map>
+#include<set>
+#include<algorithm>
+#include<queue>
+#include<random>
+#include<numeric>
+#include<functional>
+#include<stack>
+#include<cassert>
+#include<cmath>
+
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+#define rep2(i,j,n) for (int i = 0; i < (int)n; i++) for (int j = 0; j < (int)n; j++)
+#define rep3(i,j,k,n) for (int i = 0; i < (int)n; i++) for (int j = 0; j < (int)n; j++) for (int k= 0; k < (int)n; k++)
+#define all(v) v.begin(),v.end()
+
+template<typename T>
+using vec = std::vector<T>;
+template<typename T>
+using vec2 = vec<vec<T>>;
+using ll = long long;
+template<typename T>
+using t2 = std::tuple<T, T>;
+using i2 = t2<int>;
+template<typename T>
+using t3 = std::tuple<T, T, T>;
+using i3 = t3<int>;
+using clk = std::chrono::system_clock;
+using i4 = std::tuple<int,int,int,int>;
+
+template<typename T>
+void print(const T& v){
+    std::cout << v;
+}
+template<typename... T>
+void print(const std::tuple<T...>& tp);
+template<typename T>
+void print(const std::vector<T>& vs){
+    std::cout << "[";
+    rep(i, vs.size()){
+        if(i > 0) print(" ");
+        print(vs[i]);
+    }
+    std::cout << "]";
+}
+template<typename TupType, size_t... I>
+void print(const TupType& tp, std::index_sequence<I...>){
+    std::cout << "(";
+    (..., (print(I==0? "" : " "), print(std::get<I>(tp))));
+    std::cout << ")";
+}
+template<typename... T>
+void print(const std::tuple<T...>& tp){
+    print(tp, std::make_index_sequence<sizeof...(T)>());
+}
+template<class T, class... A> void print(const T& first, const A&... rest) { print(first); print(" "); print(rest...); }
+
+template<class... T>
+void dprint(const T&... rest){
+    std::cout << "# ";
+    print(rest...);
+    std::cout << "\n";
+}
+
