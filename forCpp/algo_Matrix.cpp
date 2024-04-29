b4cab46bb6a00885e5aba29397eb6dc4fac1a273
izunori <izuharakun53@yahoo.co.jp>
2024-04-29 19:21:25 +0900

    Faster

--- forCpp/algo_Matrix.cpp
+++ forCpp/algo_Matrix.cpp
@@ -1,3 +1,9 @@
+#pragma GCC optimize("O3,inline")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC target("movbe")
+#pragma GCC target("aes,pclmul,rdrnd")
 #include<iostream>
 #include<vector>
 #include<string>
@@ -24,29 +30,29 @@ public:
     int id = 0;
     int r,c;
     //vec2<T> data;
-    T** data = nullptr;
+    //T** data = nullptr;
     T* buf = nullptr;
     Mat(int r, int c):r(r),c(c){
         id = _index;
         _index++;
         //dprint("create", id, this);
-        buf = (T*)std::malloc((r*c) * sizeof(T));
-        data = (T**)std::malloc(r * sizeof(T*));
-        rep(i,r) data[i] = buf + i * c;
+        buf = (T*)std::calloc((r*c), sizeof(T));
+        //data = (T**)std::malloc(r * sizeof(T*));
+        //rep(i,r) data[i] = buf + i * c;
     }
     Mat(Mat && rhs){
         //dprint("Move");
         r = rhs.r; c = rhs.c;
         buf = rhs.buf; rhs.buf = nullptr;
-        data = rhs.data; rhs.data = nullptr;
+        //data = rhs.data; rhs.data = nullptr;
     }
     Mat& operator=(Mat&& rhs){
         r = rhs.r; c = rhs.c;
         //dprint("substitute", this, &rhs, r, c);
         buf = rhs.buf;
-        data = rhs.data;
+        //data = rhs.data;
         rhs.buf = nullptr;
-        rhs.data = nullptr;
+        //rhs.data = nullptr;
         return *this;
     }
     Mat(const Mat &rhs){
@@ -54,41 +60,43 @@ public:
         //dprint("copy", this, &rhs, r, c);
         buf = (T*)std::malloc((r*c) * sizeof(T));
         std::memcpy(buf, rhs.buf, (r*c)*sizeof(T));
-        data = (T**)std::malloc(r * sizeof(T*));
-        rep(i,r) data[i] = buf + i * c;
+        //data = (T**)std::malloc(r * sizeof(T*));
+        //rep(i,r) data[i] = buf + i * c;
     }
     ~Mat(){
-        //dprint("destroy", id, this, buf);
         if(buf != nullptr){
+            dprint("destroy", id, this, buf);
             std::free(buf);
             buf = nullptr;
         }
-        if(data != nullptr){
-            std::free(data);
-            data = nullptr;
-        }
+        //if(data != nullptr){
+        //    std::free(data);
+        //    data = nullptr;
+        //}
     }
     Mat prod(const Mat& rhs){
         Mat res(r, rhs.c);
         //dprint("prod", &res);
         rep(x,r){
-            rep(y,rhs.c){
-                T t = 0;
-                rep(i,c){
-                    t += data[x][i] * rhs.data[i][y];
+            rep(i,c){
+                rep(y,rhs.c){
+                    res.buf[x*rhs.c + y] += buf[c*x+ i] * rhs.buf[rhs.c * i + y];
                 }
-                res.data[x][y] = t;
             }
         }
         return res;
     }
+    T& operator()(const int x, const int y){
+        return buf[x*c + y];
+    }
+
 };
 
 int main(){
     using Eigen::MatrixXd;
 
-    int M = 200;
-    int Q = 10;
+    int M = 1000;
+    int Q = 1;
     Eigen::MatrixXd m = MatrixXd::Random(M,M);
     Eigen::MatrixXd m2 = MatrixXd::Random(M,M);
     {
@@ -107,8 +115,8 @@ int main(){
     {
         Mat<double> mm(M,M);
         Mat<double> mm2(M,M);
-        rep2(x,y,M) mm.data[x][y] = m(x,y);
-        rep2(x,y,M) mm2.data[x][y] = m2(x,y);
+        rep2(x,y,M) mm(x,y) = m(x,y);
+        rep2(x,y,M) mm2(x,y) = m2(x,y);
 
         auto mm3 = mm;
 
@@ -118,7 +126,7 @@ int main(){
         }
         auto end_time = clk::now();
 
-        dprint(mm3.data[0][0]);
+        dprint(mm3(0,0));
         dprint(getElapsed(start_time, end_time));
     }
 
